03898795f3cc211ebceb035c23c26dfdd0f0ba59
Mahmoud Sayed <m.sayedfci@gmail.com>
2021-04-11 23:29:49 +0200

    User logout

---
+++ resources/js/components/auth/Logout.vue
@@ -0,0 +1,23 @@
+<template>
+    <div>
+
+    </div>
+</template>
+<style scoped>
+
+</style>
+<script>
+import AppStorage from '../../Helpers/AppStorage';
+
+export default {
+    name: 'logout',
+    created() {
+        AppStorage.clear();
+        Toast.fire({
+            icon: 'success',
+            title: 'Logged out successfully'
+        });
+        this.$router.push({ name: '/' });
+    }
+}
+</script>
--- resources/js/routes.js
+++ resources/js/routes.js
@@ -1,6 +1,8 @@
 import Login from './components/auth/Login.vue';
 import Register from './components/auth/Register.vue';
 import Forgot from './components/auth/Forgot.vue';
+import Logout from './components/auth/Logout.vue';
+
 import Home from './components/Home.vue';
 
 
@@ -8,5 +10,6 @@ export const routes = [
     {path: '/', component: Login, name: '/'},
     {path: '/register', component: Register, name: 'register'},
     {path: '/forgot', component: Forgot, name: 'forgot'},
+    {path: '/logout', component: Logout, name: 'logout'},
     {path: '/home', component: Home, name: 'home'},
 ];
